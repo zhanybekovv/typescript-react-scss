2caf1954263accf6f3f165f81efcb79f237edb08
zhanybekovv <nartay.zhanybekov@nu.edu.kz>
2020-06-27 21:48:49 +0600

    feat: add header

---
+++ myApp/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap";
+import Header from './Header'
+import "./App.css"
+function App() {
+  return (
+    <div className = "d-flex justify-content-center">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
